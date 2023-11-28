5a13df116f4ecb0a09f53ddcd8f8de2b35ffd86d
RomanFisc <romanfisc@gmail.com>
2023-11-27 23:52:03 -0500

    created function operate to call the math operator functions

--- calculator.js
+++ calculator.js
@@ -18,3 +18,22 @@ const divide = function(...nums) {
 }
 //console.log(divide(20, 2, 3));
 
+let num1;
+let num2;
+let operator = '*';
+
+const operate = function(num1, num2) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === '/') {
+        return divide(num1,num2);
+    } else if (operator === '*') {
+        return multiply(num1,num2);
+    };
+}
+
+console.log(operate(4,5));
+
+
